693a6db1b0d886180b9c84701b69bec8c8f41e6c
rufunt <jazzoo@mail.ru>
2018-03-13 19:09:18 +0000

    Add rating star

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,9 @@
 class Course < ActiveRecord::Base
+   def star_number
+     self.star.blank? ? 1 : self.star
+   end
+   
+   def cover
+     self.image.blank? ? "default.jpg" : self.image
+   end
 end
