aa6b50d59e0a742b95ee348d478153d060f5f3d7
rufunt <jazzoo@mail.ru>
2018-03-13 16:48:47 +0000

    Add home page and change root

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   resources :courses
   get 'pages/about'
-
+  
   get 'pages/location'
   
-  root 'courses#index'
-
+  root "pages#home"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
